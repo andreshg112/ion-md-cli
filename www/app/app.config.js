fc243e7c7d920b6b1c3c92fe7f1a9c40f3af4d72
andreshg112 <andreshg112@gmail.com>
2016-09-10 18:49:49 -0500

    1. Se borraron archivos no utilizados. img, controllers y templates.

--- www/app/app.config.js
+++ www/app/app.config.js
@@ -79,51 +79,6 @@
                         redirectTo: 'app.pedidos'
                     }
                 }
-            })
-            .state('app.lists', {
-                url: '/lists',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'templates/lists.html',
-                        controller: 'ListsCtrl'
-                    }
-                }
-            })
-            .state('app.ink', {
-                url: '/ink',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'templates/ink.html',
-                        controller: 'InkCtrl'
-                    }
-                }
-            })
-            .state('app.motion', {
-                url: '/motion',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'templates/motion.html',
-                        controller: 'MotionCtrl'
-                    }
-                }
-            })
-            .state('app.components', {
-                url: '/components',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'templates/components.html',
-                        controller: 'ComponentsCtrl'
-                    }
-                }
-            })
-            .state('app.extensions', {
-                url: '/extensions',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'templates/extensions.html',
-                        controller: 'ExtensionsCtrl'
-                    }
-                }
             });
 
         // if none of the above states are matched, use this as the fallback

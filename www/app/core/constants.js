3512378f711f2ade41a24c943645b686222766b5
andreshg112 <andreshg112@gmail.com>
2016-10-25 23:28:05 -0500

    Corregido error al presionar Esc quitaba pedido.

--- www/app/core/constants.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    'use strict';
-
-    angular.module('app')
-        // .constant('API', 'http://api.fidelivery.co/ion-md-server/public')
-        .constant('API', 'http://localhost/ion-md-server/public')
-        ;
-})();
